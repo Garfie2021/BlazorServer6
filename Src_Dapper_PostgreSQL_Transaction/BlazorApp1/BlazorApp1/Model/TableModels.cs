a298f20030ddb8223c0bb3945ea687a14f3d37cc
ukami <ukami@interserve.co.jp>
2023-01-11 19:04:24 +0900

    1

---
+++ Src_Dapper_PostgreSQL_Transaction/BlazorApp1/BlazorApp1/Model/TableModels.cs
@@ -0,0 +1,13 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace BlazorApp1.Model
+{
+    public class m_table_a
+    {
+        public long id;
+        public string code;
+        public string? value_string;
+        public DateTime? value_date;
+    }
+
+}
